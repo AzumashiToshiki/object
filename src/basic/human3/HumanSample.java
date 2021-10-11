39850a40b1739fdc6144bb78efeb30e59ec73bc9
504a <moritaka@wdc-kd.com>
2021-10-11 12:19:42 +0900

    20211011

---
+++ src/basic/human3/HumanSample.java
@@ -0,0 +1,10 @@
+package basic.human3;
+
+class HumanSample {
+    public static void main(String[] args) {
+        Human tom = new Human("Tom");
+        tom.sayHello();
+        Japanese hiroshi = new Japanese("ひろし");
+        hiroshi.sayHello();
+    }
+}
---
+++ src/basic/human3/Japanese.java
@@ -0,0 +1,9 @@
+package basic.human3;
+
+//日本人クラス
+class Japanese extends Human{
+
+    public Japanese(String name) {
+        super(name);
+    }
+}
