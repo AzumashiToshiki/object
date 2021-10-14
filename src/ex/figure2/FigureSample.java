91934d3ebf8a9fab9642fd58d36204efbb4cf186
504a <moritaka@wdc-kd.com>
2021-10-14 14:22:24 +0900

    20211014

---
+++ src/ex/figure2/FigureSample.java
@@ -0,0 +1,19 @@
+package ex.figure2;
+
+//図形の面積
+class FigureSample {
+    public static void main(String[] args) {
+        //インスタンスの作成
+        Figure[] figures = {
+                new Rectangle(10,20),
+                new Triangle(20,15),
+                new Circle(10)
+        };
+
+        //出力
+        for (Figure figure:figures) {
+            System.out.println(figure.getArea());
+            //System.out.println(figure);
+        }
+    }
+}
