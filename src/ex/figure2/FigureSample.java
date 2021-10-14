b868bebc7f4476c88c4200bb35f95534deb82153
504a <moritaka@wdc-kd.com>
2021-10-14 14:51:52 +0900

    20211014

--- src/ex/figure2/FigureSample.java
+++ src/ex/figure2/FigureSample.java
@@ -1,26 +1,23 @@
 package ex.figure2;
 
-import basic.figure2.Circle;
-import basic.figure2.Rectangle;
-import basic.figure2.Triangle;
-
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 //図形の面積
 class FigureSample {
     public static void main(String[] args) {
         //インスタンスの作成
         //ArrayListで宣言
-        List<basic.figure2.Figure> figures = new ArrayList<>();
+        List<Figure> figures = new ArrayList<>();
         figures.addAll(Arrays.asList(
-                new basic.figure2.Rectangle(10,20),
-                new basic.figure2.Triangle(20,15),
-                new basic.figure2.Circle(10)
+                new Rectangle(10,20),
+                new Triangle(20,15),
+                new Circle(10)
         ));
 
         //出力
-        for (basic.figure2.Figure figure:figures) {
+        for (Figure figure:figures) {
             System.out.println(figure);
 
         }
