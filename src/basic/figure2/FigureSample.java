912800ce016e9b356b41abfad837470557713dc8
504a <moritaka@wdc-kd.com>
2021-10-18 09:45:17 +0900

    20211018

--- src/basic/figure2/FigureSample.java
+++ src/basic/figure2/FigureSample.java
@@ -8,7 +8,6 @@ import java.util.List;
 class FigureSample {
     public static void main(String[] args) {
         //インスタンスの作成
-        //インスタンスの作成
         Figure[] figures = {
                 new Rectangle(10,20),
                 new Triangle(20,15),
